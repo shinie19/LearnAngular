b09b4243d42df36fd35acd9b20d145dc7a6d5dda
shinie19 <nguyenanh.binoo@gmail.com>
2022-07-20 09:02:38 +0700

    Finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ArticleComponent } from './features/article/article.component';
 import { LikeComponentComponent } from './features/like-component/like-component.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent, ArticleComponent, LikeComponentComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
