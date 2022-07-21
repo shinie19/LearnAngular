1a2e2aa512444ca0d1d7dd344647e852b7777d97
shinie19 <nguyenanh.binoo@gmail.com>
2022-07-21 15:45:18 +0700

    Finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImageGalleryModule } from './image-gallery/image-gallery.module';
 import { GalleryConfig } from './image-gallery/token';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ImageGalleryModule],
+  imports: [BrowserModule, AppRoutingModule, ImageGalleryModule, SharedModule],
   providers: [{ provide: GalleryConfig, useValue: 3 }],
   bootstrap: [AppComponent],
 })
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SharedRoutingModule } from './shared-routing.module';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+@NgModule({
+  declarations: [NavBarComponent],
+  imports: [CommonModule, SharedRoutingModule],
+  exports: [NavBarComponent],
+})
+export class SharedModule {}
